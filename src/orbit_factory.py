aae46c3c87c0a9d0e4ed85984a8ec1ae44977cb0
Michelle Li <mwl9639@nyu.edu>
2024-05-14 14:40:33 -0400

    Made commits to the orbit class and orbit factory

--- src/orbit_factory.py
+++ src/orbit_factory.py
@@ -4,6 +4,8 @@ class OrbitFactory:
     @staticmethod
     def create_orbit(L, E, Veff):
         orbit = Orbit(L, E, Veff)
+        if E > Veff.Eu:
+            raise ValueError("problem")
 
         return orbit
     
