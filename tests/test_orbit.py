b011fcd7aac2a84e92e6b0139cce28350e6c6487
Michelle Li <mwl9639@nyu.edu>
2024-05-15 08:56:05 -0400

    Changed code for test orbit so that it tests orbit factory taking L, E, category as arguments rather than rmin, rmax, Veff

--- tests/test_orbit.py
+++ tests/test_orbit.py
@@ -5,11 +5,9 @@ from orbit_factory import OrbitFactory
 def test_orbit_factory():
     L = 3.0
     E = 1.5
-    rmin = 2.0
-    rmax = 10.0
-    Veff = 2.0 # in actual code this will be a function, but we will use a number for testing purposes
+    category = 1
 
-    created_orbit = OrbitFactory.create(L, E, rmin, rmax, Veff)
+    created_orbit = OrbitFactory.create(L, E, category)
 
     assert isinstance(created_orbit, Orbit)
 
