bc47e37c00d4f85e77cf264035f26824f2913161
Michelle Li <mwl9639@nyu.edu>
2024-05-15 09:01:22 -0400

    Made necessary edits to orbit factory and orbit to reflect changes in test orbit regarding arguments passed into class

--- src/orbit.py
+++ src/orbit.py
@@ -3,11 +3,11 @@ from v_eff import Veff
 
 class Orbit():
 
-    def __init__(self, L, E, Veff):
+    def __init__(self, L, E, rmin, rmax, Veff):
         self.L = L
         self.E = E
-        self.Veff = Veff(L)
-        self.roots = self.Veff.REGISTRY[E]
-        self.circ = None  # checks if it is a circle
+        self.rmin = rmin
+        self.rmax = rmax
+        self.Veff = Veff
 
 
--- src/orbit_factory.py
+++ src/orbit_factory.py
@@ -1,11 +1,23 @@
 from orbit import Orbit
+from v_eff import Veff
 
 class OrbitFactory:
     @staticmethod
-    def create_orbit(L, E, Veff):
-        orbit = Orbit(L, E, Veff)
-        if E > Veff.Eu:
-            raise ValueError("problem")
+    def create_orbit(L, E, category):
+        if category == "category 1":
+            veff = Veff(L)
+            if E in veff.REGISTRY:
+                rmin, rmax = veff.REGISTRY[E]
+            else:
+                rmin, rmax = veff.calc_turning_points(E)
+        elif category == "category 2":
+            pass
+        elif category == "category 3":
+            pass
+        elif category == "category 4":
+            pass
+
+        orbit = Orbit(L, E, rmin, rmax, veff)
 
         return orbit
     
