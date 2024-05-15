291a77e64a77d0847f191ab862f1644d59ab6451
Mickey Shamah <mickeyshamah@10-18-214-37.dynapool.wireless.nyu.edu>
2024-05-15 14:22:51 -0400

    Added driver tests

---
+++ tests/test_driver.py
@@ -0,0 +1,44 @@
+#driver test
+import pytest
+import yaml
+
+from src.orbit import orbit
+from src.v_eff import v_eff
+import driver
+
+
+
+
+# test YAML data as a string
+yaml_data = """
+L: 4.5
+E: .0979795
+C: 4
+"""
+
+
+@pytest.fixture
+def loaded_yaml_data():
+    return yaml.safe_load(yaml_data)
+
+# Test to check YAML data is correct
+def test_model_name():
+    processed_data = yaml.safe_load(yaml_data)
+    
+    # Assert the model name is as expected
+    assert isinstance(processed_data, dict)
+
+    assert processed_data['L'] == 4.5, "L doesnt match"
+    assert processed_data['E'] == 0.0979795, "E doesnt match"
+
+
+# testing driver code functions
+
+def test_orbit(loaded_yaml_data):
+    o = driver.create_orbit(loaded_yaml_data)
+    assert isinstance(o, orbit) 
+    
+def test_veff(loaded_yaml_data):
+    veff = driver.create_veff(loaded_yaml_data)
+    assert isinstance(veff, v_eff)
+
