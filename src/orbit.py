53a116d8224138c65e661a077f9e921be6cc8534
Michelle Li <mwl9639@nyu.edu>
2024-05-15 12:59:24 -0400

    Edits to orbit and orbit factory changing the way we retrieve rmin and rmax values

--- src/orbit.py
+++ src/orbit.py
@@ -2,8 +2,18 @@ from v_eff import Veff
 
 
 class Orbit():
+    '''
+    Class to represent an orbit object which holds the following attributes.
 
-    def __init__(self, L, E, rmin, rmax, Veff):
+    Attributes:
+    L (float): Angular momentum of the orbiting body.
+    E (float): Energy of the orbiting body.
+    rmin (float): Minimum radius of the orbit.
+    rmax (float): Maximum radius of the orbit.
+    Veff (Veff): Effective potential function for the orbit.
+    '''
+
+    def __init__(self, L: float, E: float, rmin: float, rmax: float, Veff: Veff):
         self.L = L
         self.E = E
         self.rmin = rmin
--- src/orbit_factory.py
+++ src/orbit_factory.py
@@ -1,21 +1,45 @@
 from orbit import Orbit
 from v_eff import Veff
+from v_eff_factory import VeffFactory
 
 class OrbitFactory:
+    '''
+     A factory class for creating Orbit objects based on specified parameters and categories.
+    '''
     @staticmethod
-    def create_orbit(L, E, category):
-        if category == "category 1":
-            veff = Veff(L)
-            if E in veff.REGISTRY:
-                rmin, rmax = veff.REGISTRY[E]
+    def create_orbit(L: float, E: float, category: int) -> Orbit:
+        '''
+        Creates an Orbit object based on the angular momentum, energy, and category.
+
+        Parameters:
+        L (float): Angular momentum of the orbiting body.
+        E (float): Energy of the orbiting body.
+        category (int): Category determining the type of orbit to create.
+
+        Returns:
+        Orbit: An Orbit object with the specified parameters.
+
+        Raises:
+        ValueError: If the category is not recognized or if the required roots are not found.
+        '''
+        if category == 1:
+            pass
+        elif category == 2:
+            pass
+        elif category == 3:
+            pass
+        elif category == 4:
+            veff_factory = VeffFactory()
+            veff = veff_factory.create(L)
+            roots = veff.calc_turning_points(E)
+            if len(roots) == 2:
+                rmin, rmax = roots
+            elif len(roots) == 3:
+                rmin, rmax = roots[1:]
             else:
-                rmin, rmax = veff.calc_turning_points(E)
-        elif category == "category 2":
-            pass
-        elif category == "category 3":
-            pass
-        elif category == "category 4":
-            pass
+                raise ValueError("Invalid number of turning points found for category 4.")
+        else:
+            raise ValueError("Unrecognized category.")
 
         orbit = Orbit(L, E, rmin, rmax, veff)
 
