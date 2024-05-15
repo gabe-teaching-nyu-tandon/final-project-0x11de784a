3e51b69a83788c2b28ab2ede398bccfb2d66459f
Michelle Li <mwl9639@nyu.edu>
2024-05-15 12:58:50 -0400

    Added a test to test correct rmin and rmax values made from orbit factory

--- tests/test_orbit.py
+++ tests/test_orbit.py
@@ -1,15 +1,27 @@
-from orbit import Orbit
+from src.orbit import Orbit
 from orbit_factory import OrbitFactory
+from v_eff_factory import VeffFactory
+import numpy as np
 
 
-def test_orbit_factory():
+def test_creating_orbit():
     L = 3.0
     E = 1.5
-    category = 1
+    category = 4
 
     created_orbit = OrbitFactory.create(L, E, category)
 
     assert isinstance(created_orbit, Orbit)
 
+
+def test_orbit_factory():
+    L = 4.5
+    E = 0.979795
+    orbit = OrbitFactory.create(L, E, 4)
+
+    assert np.allclose(orbit.rmin, 10.2518)
+    assert np.allclose(orbit.rmax, 37.0828)
+
+    
 
 
